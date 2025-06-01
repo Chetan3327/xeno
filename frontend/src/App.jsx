dd082320a361cd6945dcee52b584dcd8a56e3873
Chetan3327 <chauhanchetan12789@gmail.com>
2025-06-01 12:43:27 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
+import Login from '../pages/login.jsx'
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
